0925fb6b380a09805a3c48336a632e7576b66473
lassoued iheb <iheb.lassoued.1@esprit.tn>
2020-11-02 14:22:27 +0100

    commm

--- src/test/java/tn/esprit/spring/services/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/EmployeServiceImplTest.java
@@ -33,7 +33,11 @@ public class EmployeServiceImplTest {
 		assertEquals(employe.getNom(), addedEmp.getNom());
 	}
 	
-
+	@Test
+	public void testGetAllEmployes() {
+		assertEquals(6, iEmployeService.getAllEmployes().size());
+	}
+	
 	@Test
 	public void testdeleteEmployeById() {
 		Employe employe = new Employe("Aymen", "lassoued", "iheb.lassoued@esprit.tn", true, Role.INGENIEUR);
