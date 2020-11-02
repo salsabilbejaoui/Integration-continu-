aedca64531defb7e23f85c3d0e197cb744a48550
lassoued iheb <iheb.lassoued.1@esprit.tn>
2020-11-02 03:58:03 +0100

    commit 1

--- src/main/java/tn/esprit/spring/services/IEmployeService.java
+++ src/main/java/tn/esprit/spring/services/IEmployeService.java
@@ -15,13 +15,13 @@ public interface IEmployeService {
 	public Employe authenticate(String login, String password) ;
 	 
 	
-	public void mettreAjourEmailByEmployeId(String email, int employeId);
+	public Employe mettreAjourEmailByEmployeId(String email, int employeId);
 	public void affecterEmployeADepartement(int employeId, int depId);
 	public void desaffecterEmployeDuDepartement(int employeId, int depId);
 	public int ajouterContrat(Contrat contrat);
 	public void affecterContratAEmploye(int contratId, int employeId);
 	public String getEmployePrenomById(int employeId);
-	public void deleteEmployeById(int employeId);
+	public Boolean deleteEmployeById(int employeId);
 	public void deleteContratById(int contratId);
 	public int getNombreEmployeJPQL();
 	public List<String> getAllEmployeNamesJPQL();
@@ -34,7 +34,7 @@ public interface IEmployeService {
 	public List<Timesheet> getTimesheetsByMissionAndDate(Employe employe, Mission mission, 
 	Date dateDebut, Date dateFin);
 
-	int addOrUpdateEmploye(Employe employe);
+	Employe addOrUpdateEmploye(Employe employe);
 	
 	
 	
