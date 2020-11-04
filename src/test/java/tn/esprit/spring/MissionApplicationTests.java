a056ae1d4aa46370b42ac15238c4ebc8b1cf6cbe
amir.akari@esprit.tn <amir.akari@esprit.tn>
2020-11-04 17:20:50 +0100

    erreur

--- src/test/java/tn/esprit/spring/MissionApplicationTests.java
+++ src/test/java/tn/esprit/spring/MissionApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
