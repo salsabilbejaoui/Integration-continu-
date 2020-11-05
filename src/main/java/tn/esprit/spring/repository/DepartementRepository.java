4413f0162c7f75f5a33a74c449fd2fef63ba5c42
MasmoudiOmar <masmoudi.omaar@gmail.com>
2020-11-05 02:11:43 +0100

    second commit that has nexus working

--- src/main/java/tn/esprit/spring/repository/DepartementRepository.java
+++ src/main/java/tn/esprit/spring/repository/DepartementRepository.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,6 @@ import tn.esprit.spring.entities.Departement;
 
 @Repository
 public interface DepartementRepository extends CrudRepository<Departement, Integer>{
-
+	@Query("SELECT count(*) FROM Departement")
+    public int countdep();
 }
